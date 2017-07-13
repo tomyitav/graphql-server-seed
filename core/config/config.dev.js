fc31cddf77856343eb69d1b8cd47989df3ebd87d
Tom <tomyitav@gmail.com>
2017-07-14 00:18:59 +0300

    Using process env in config

--- core/config/config.dev.js
+++ core/config/config.dev.js
@@ -4,8 +4,8 @@ let config = {};
 
 config.logFileDir = path.join(__dirname, '../../log');
 config.logFileName = 'app.log';
-config.dbHost = 'localhost';
-config.dbPort = '27017';
-config.dbName = 'trains';
+config.dbHost = process.env.dbHost || 'localhost';
+config.dbPort = process.env.dbPort || '27017';
+config.dbName = process.env.dbName || 'trains';
 
 export default config;
